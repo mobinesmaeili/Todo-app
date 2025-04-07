06deafa22ac87ddc8f31923ee3a79ed6edabf63c
Mobin <mobin.esmaeili00@gmail.com>
2025-04-07 03:32:24 +0330

    add delete btn to details page

--- A/home/urls.py
+++ A/home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('detail/<int:todo_id>/', views.detail, name='detail'),
+    path('delete/<int:todo_id>/', views.delete, name='delete'),
 ]

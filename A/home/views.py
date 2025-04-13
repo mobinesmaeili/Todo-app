00a556eeb8db99d604242b6042ba8cb7e9b7bf14
Mobin <mobin.esmaeili00@gmail.com>
2025-04-13 22:29:50 +0330

    add Django form to template

--- A/home/views.py
+++ A/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from home.models import *
 from django.contrib import messages
+from .forms import TodoCreateForm
 
 
 def home(request):
@@ -15,3 +16,15 @@ def delete(request, todo_id):
     Todo.objects.get(id=todo_id).delete()
     messages.success(request, 'todo deleted successfully', 'success')
     return redirect('home')
+
+def create(request):
+    if request.method == 'POST':
+        form = TodoCreateForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            Todo.objects.create(title=cd['title'], body=cd['body'], created_at=cd['created_at'])
+            messages.success(request, 'Todo created successfully', 'success')
+            return redirect('home')
+    else:
+        form = TodoCreateForm()
+    return render(request, 'create.html', context={'form': form})
